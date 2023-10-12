61941afb612767fb57b2f890690e928a7f3b68f6
Yoseline Angel <61163800+yoselineangel@users.noreply.github.com>
2023-10-12 10:15:55 -0400

    Update neon_get_loc.py
    Updated to latest Hytools version, still requires manually setting input and output paths.

--- neon_get_loc.py
+++ neon_get_loc.py
@@ -9,10 +9,14 @@ Created on Mon Sep 12 14:29:31 2022
 import sys
 import numpy as np
 import h5py
-import gdal, osr, os
+from osgeo import gdal, osr
+import os
 import matplotlib.pyplot as plt
 import warnings
+from collections import OrderedDict
 from spectral import *
+
+from pyproj import Transformer
 
 warnings.filterwarnings('ignore')
 
@@ -44,13 +48,50 @@ for filename in os.listdir(directory):
         #print(igm)
         
         loc = np.array(igm)
-        md = {'lines': igm.shape[0],
-          'samples': igm.shape[1],
-          'bands': igm.shape[2],
-          'header offset' : 0,
-          'data ignore value' : -9999,
-          'band names': {'1East','2North','3Elevation'}}
-        name = filename[:-3]
-        outpath= '/Users/yangello/Documents/Data/NEON_rad_sensor_ortho_line/Envi/'
-        envi.save_image(outpath+name+'_loc.hdr', loc, dtype=np.float64, interleave = 'bil', byteorder = 0, metadata = md)
         
+        ####
+        #east
+        x = loc[:,:,0].flatten()
+        #north
+        y = loc[:,:,1].flatten()
+        #elevation
+        z = loc[:,:,2]
+        
+        transformer = Transformer.from_crs("epsg:32618",
+                                   "epsg:4326",
+                                   always_xy=True,
+                                  )
+        lon, lat = transformer.transform(x, y)
+                
+        #mask = (z==-9999)
+        #z[mask] = np.nan
+        
+        lon_layer = lon.reshape(loc.shape[0],loc.shape[1])
+        lat_layer = lat.reshape(loc.shape[0],loc.shape[1])
+        
+        #lon_layer[mask] = np.nan
+        #lat_layer[mask] =np.nan
+        
+        new_loc = np.dstack((lon_layer, lat_layer, z))
+        ####
+        
+        #md = {'lines': igm.shape[0],
+          #'samples': igm.shape[1],
+          #'bands': igm.shape[2],
+          #'header offset' : 0,
+          #'data ignore value' : -9999,
+          #'band names': {'1','2','3'}}
+        
+        name = filename[:-3]
+        
+        mt = OrderedDict()
+        mt['lines'] = igm.shape[0]
+        mt['samples'] = igm.shape[1]
+        mt['bands'] = igm.shape[2]
+        mt['header offset'] = 0
+        mt['data ignore value'] = -9999
+        mt['band names'] = ['East','North','Elevation']
+        
+        outpath= '/Users/yangello/Documents/Data/NEON_rad_sensor_ortho_line/Envi/'
+        #envi.save_image(outpath+name+'_loc.hdr', loc, dtype=np.float64, interleave = 'bil', byteorder = 0, metadata = mt)
+        envi.save_image(outpath+name+'_loc.hdr', new_loc, dtype=np.float64, interleave = 'bil', byteorder = 0, metadata = mt)
