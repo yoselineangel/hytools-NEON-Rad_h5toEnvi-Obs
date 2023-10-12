0b8469d2cda53a72a450589674d2646d09f11cbf
Yoseline Angel <61163800+yoselineangel@users.noreply.github.com>
2023-10-12 10:10:00 -0400

    Update Modifying_h5_rad.py
    Fixes dtype issues but requires manually setting input and output paths.

--- Modifying_h5_rad.py
+++ Modifying_h5_rad.py
@@ -9,15 +9,16 @@ Created on Fri Sep  2 16:44:11 2022
 import sys
 import numpy as np
 import h5py
-import gdal, osr, os
-import matplotlib.pyplot as plt
+#import gdal, osr, os
+import os
+#import matplotlib.pyplot as plt
 import warnings
 
 warnings.filterwarnings('ignore')
 
 # assign directory
 # modify directory to your own path where you store the h5 radiance files
-directory = '/Users/yangello/Documents/Data/NEON_rad_sensor-ortho-line/h5/'
+directory = '/Users/yangello/Documents/Data/NEON_rad_sensor_ortho_line/h5/'
 
 
 # iterate over files in that directory
@@ -32,7 +33,16 @@ for filename in os.listdir(directory):
         rad_dec = f['SERC']['Radiance']['RadianceDecimalPart']
         rad_int = f['SERC']['Radiance']['RadianceIntegerPart']
         rad = (rad_int[:,:,:] + (rad_dec [:,:,:]/50000))
-        rad[rad > 255] = -9999
-        rad = rad.astype(np.float32)
-        f.create_dataset('/SERC/Radiance/Radiance_total', data=rad, dtype=np.float32)
+        #rad[rad > 255] = -9999
+        rad[rad > 255] = np.nan
+        rad = rad.astype('f4')
+        
+     
+        print('Start writing new file')
+        dset = f.create_dataset('/Users/yangello/Documents/Data/NEON_rad_sensor_ortho_line/Envi/Radiance/Radiance_total', data=rad, dtype='f4')
+        
+        dset.attrs['Dimension_Labels'] = ['Line', 'Sample', 'Wavelength']
+        dset.attrs['Interleave'] = 'bil'
+        
+        
         f.close()
